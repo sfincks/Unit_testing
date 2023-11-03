dfea136206cba72b80fc386a203e2c8d8cfe87c0
sfincks <sfincks666@gmail.com>
2023-11-03 18:56:39 +0500

    init

---
+++ src/main/java/Hw1/Hw1.java
@@ -0,0 +1,8 @@
+package Hw1;
+import static org.junit.Assert.*;
+
+public class Hw1 {
+
+
+
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+import static org.junit.Assert.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+
+    }
+}
